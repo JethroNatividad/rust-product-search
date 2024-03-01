0744460ae74841a58550e293a7eeb03f0d799eaa
JethroNatividad <natividad.jet@gmail.com>
2024-03-01 08:49:36 +0800

    write pseudocode

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 // Inputs: product name
 // Process: parse json file, search for str
 // Outputs: price and quantity if found.
+
+// Create struct for json
 fn main() {
-    println!("Hello, world!");
+    // Read the file "source/products.json"
+    // parse the JSON file
+    // get input
+    // search in products
+    // if not found, Output "Sorry, that product was not found in our inventory."
+    // if found, show price, and quantity on hand.
 }
