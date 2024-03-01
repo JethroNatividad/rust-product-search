27cc9eac5a8e33111c5937ef1089d9c9610e0ed1
JethroNatividad <natividad.jet@gmail.com>
2024-03-01 08:43:35 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // A program that takes a product name, searches it in a json file, outputs the price and quantity.
+// Inputs: product name
+// Process: parse json file, search for str
+// Outputs: price and quantity if found.
 fn main() {
     println!("Hello, world!");
 }
